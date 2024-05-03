ef0b83b25c14386239e0a049a97a3e3da8ab660b
DoronShafrir <88454984+DoronShafrir@users.noreply.github.com>
2024-05-03 17:42:52 +0300

    Add files via upload

--- FileOreder.py
+++ FileOreder.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import openpyxl
 
 
 
@@ -6,7 +7,7 @@ import pandas as pd
 
 class Order:
     def __init__(self):
-        self.gen = pd.read_csv("General2023.csv", parse_dates=[0], encoding='iso-8859-8')
+        self.gen = pd.read_csv("General2023C.csv", parse_dates=[0], encoding='iso-8859-8')
 
         self.Jan = pd.DataFrame(columns=self.gen.columns)
         self.Feb = pd.DataFrame(columns=self.gen.columns)
@@ -40,43 +41,46 @@ class Order:
         for i, r in self.gen.iterrows():
             month_number = r['תאריך'].month
             self.add_line(r, month_number )
-            print(i)
+
 
 
     def add_line(self, line, month):
         if month == 1:
-             self.Jan = self.Jan.append(line, ignore_index=True)
-        elif month == 2:
-            self.Feb = self.Feb.append(line, ignore_index=True)
-        elif month == 3:
-            self.Mar = self.Mar.append(line, ignore_index=True)
-        elif month == 4:
-            self.Apr = self.Apr.append(line, ignore_index=True)
-        elif month == 5:
-            self.May = self.May.append(line, ignore_index=True)
-        elif month == 6:
-            self.Jun = self.Jun.append(line, ignore_index=True)
-        elif month == 7:
-            self.Jul = self.Jul.append(line, ignore_index=True)
-        elif month == 8:
-            self.Aug = self.Aug.append(line, ignore_index=True)
-        elif month == 8:
-            self.Aug = self.Aug.append(line, ignore_index=True)
-        elif month == 9:
-            self.Sep = self.Sep.append(line, ignore_index=True)
-        elif month == 10:
-            self.Oct = self.Oct.append(line, ignore_index=True)
-        elif month == 11:
-            self.Nov = self.Nov.append(line, ignore_index=True)
-        elif month == 12:
-            self.Dec = self.Dec.append(line, ignore_index=True)
+             # self.Jan = self.Jan.append(line, ignore_index=True)
+            self.Jan = pd.concat([self.Jan, line])
+            # print(self.Jan)
+        # elif month == 2:
+        #     self.Feb = self.Feb.append(line, ignore_index=True)
+        # elif month == 3:
+        #     self.Mar = self.Mar.append(line, ignore_index=True)
+        # elif month == 4:
+        #     self.Apr = self.Apr.append(line, ignore_index=True)
+        # elif month == 5:
+        #     self.May = self.May.append(line, ignore_index=True)
+        # elif month == 6:
+        #     self.Jun = self.Jun.append(line, ignore_index=True)
+        # elif month == 7:
+        #     self.Jul = self.Jul.append(line, ignore_index=True)
+        # elif month == 8:
+        #     self.Aug = self.Aug.append(line, ignore_index=True)
+        # elif month == 8:
+        #     self.Aug = self.Aug.append(line, ignore_index=True)
+        # elif month == 9:
+        #     self.Sep = self.Sep.append(line, ignore_index=True)
+        # elif month == 10:
+        #     self.Oct = self.Oct.append(line, ignore_index=True)
+        # elif month == 11:
+        #     self.Nov = self.Nov.append(line, ignore_index=True)
+        # elif month == 12:
+        #     self.Dec = self.Dec.append(line, ignore_index=True)
+
+    def create_file(self):
+        self.Jan.to_csv("Jan.csv", encoding='iso-8859-8')
 
 
-    def print_month(self):
-        print(self.Dec)
 
 
 if __name__ == "__main__":
     nava = Order()
     nava.seperate_lines()
-    nava.print_month()
+    nava.create_file()
