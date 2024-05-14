d25c2d9b3f543e589cf6268d8dce361cbfa3d14a
Doron Shafrir <doron.shafrir.74@gmail.com>
2024-05-14 08:48:40 +0300

    Work version with remove duplication, sort and family and first name seperation

--- FileOrderN.py
+++ FileOrderN.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import openpyxl
-
+import numpy as np
 class Order:
     def __init__(self):
         self.gen = pd.read_csv("General2023.csv", dayfirst=True, parse_dates=[0], encoding='iso-8859-8')
         self.gen['תאריך'] = self.gen['תאריך'].dt.date
+
+        self.gen['C'] = np.nan
+        self.gen.insert(loc=1, column='Name1', value=self.gen['C'])
+        self.gen.insert(loc=1, column='Name2', value=self.gen['C'])
 
         self.Jan = pd.DataFrame()
         self.Feb = pd.DataFrame()
@@ -26,17 +30,23 @@ class Order:
 
     def seperate_lines(self):
         for i, line in self.gen.iterrows():
-            month_number = line['תאריך'].month -1
+            month_number = line['תאריך'].month - 1
             self.months[month_number] = pd.concat([self.months[month_number], line], ignore_index=True, axis=1)
 
     def sort_lines(self):
         for i in range(12):
             self.months[i] = self.months[i].transpose()
             if not self.months[i].empty:
-                # self.months[i] = self.months[i].sort_values(by=self.months[i].iloc[:, 0])
+                # self.months[i] = self.months[i].sort_values(by= self.months[i].iloc[:, 0])
                 self.months[i] = self.months[i].sort_values(by=['סטטוס טיפול התקבל/לא התקבל/סיום'], ascending=False)
                 self.months[i] = self.months[i].drop_duplicates(subset=['שם משפחה+ שם פרטי'], keep='first')
-                
+                self.months[i] = self.months[i].assign(Name1=lambda x: (x['שם משפחה+ שם פרטי']).str.split().str[0])
+                self.months[i] = self.months[i].assign(Name2=lambda x: (x['שם משפחה+ שם פרטי']).str.split().str[1:].str.join(' '))
+                self.months[i].rename(columns={'Name1' : 'שם פרטי'}, inplace=True)
+                self.months[i].rename(columns={'Name2' : 'שם משפחה'}, inplace=True)
+
+
+
     def create_file(self):
         with pd.ExcelWriter('Year2023.xlsx') as writer:
             for i in range(12):
@@ -48,5 +58,3 @@ if __name__ == "__main__":
     nava.seperate_lines()
     nava.sort_lines()
     nava.create_file()
-
-
